74df041d56a09248ed7cde25d561d75b400abb34
chaidir04121990 <45603298+chaidir04121990@users.noreply.github.com>
2022-11-14 00:50:26 +0700

    initial commit

---
+++ src/test/java/TestCases.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestCases {
+    WebDriver driver=null;
+
+
+    @Test(priority = 1)
+    void Test1(){
+//        String Title = driver.getTitle();
+//        Integer Title_Length = driver.getTitle().length();
+        System.out.println("its Test1");
+        System.setProperty("webdriver.chrome.driver", "/Users/chaidir/eclipse/java-2021-06/Chromedriver/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+
+        driver = new ChromeDriver(options);
+        driver.get("https://www.sekolahqa.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        System.out.println("Title: "+driver.getTitle());
+        System.out.println("length: "+driver.getTitle().length());
+        System.out.println("page source: "+driver.getPageSource());
+        System.out.println("page source length: "+driver.getPageSource().length());
+
+        driver.close();
+    }
+    @Test(priority = 2)
+    void Test2(){
+        System.out.println("its Test2");
+    }
+}
